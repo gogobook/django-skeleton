3871e498c0723a908190fb5d4a403b5c49f9d0c4
Nikita Khlestov <khlestovnikita@gmail.com>
2016-03-05 01:42:36 +0700

    added apps to the python path

--- conf/base.py
+++ conf/base.py
@@ -5,6 +5,9 @@ import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ROOT_DIR = os.path.dirname(BASE_DIR)
+sys.path.append(
+    os.path.join(BASE_DIR, 'apps')
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -27,7 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.core',
+    'core',
 ]
 
 MIDDLEWARE_CLASSES = [
