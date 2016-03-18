090b2d6a1c24d05101d34b3ff1ef609e8cf7a297
Nikita Khlestov <khlestovnikita@gmail.com>
2016-03-18 14:09:51 +0200

    fix for more correct deploy

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ from sys import path
 SITE_ROOT = dirname(dirname(abspath(__file__)))
 path.append(SITE_ROOT)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.conf")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.conf")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
